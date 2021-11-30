5930c6d7377c02d3bc21c7370e0f8eeb22b9a45b
Anthony Zhu <anthonyzhu268524@gmail.com>
2021-11-30 18:56:19 +0800

    update 11/30/2021

--- Project.py
+++ Project.py
@@ -2,129 +2,31 @@ import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 import pandas as pd
+import time
+from math import radians, cos, sin, asin, sqrt
 
-class Vehicle():
-    def __init__(self, name, cap, totOprTime, oprCost):
-        self.name = name
-        self.cap = cap
-        self.totOprTime = totOprTime
-        self.oprCost = oprCost
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    def __str__(self):
-        return f"Vehicle: {self.name}\n  Capacity: {self.cap}\n  Total Operation Time: {self.totOprTime}\n \
-            Operation Cost: {self.oprCost}"
-
-class Demand():
-    def __init__(self, name, capacity, duration):
-        self.name = name
-        self.capacity = capacity
-        self.dur = duration
-
-    def __str__(self):
-        about = f"Demand: {self.name}\n  Capacity: {self.capacity}\n  Duration: {self.dur}"
-        return about
-
-class Patient():
-    def __init__(self, name, locX, locY, tStart, tEnd, demandType, demand, dur):
-        self.name = name
-        self.loc = np.array([locX,locY])
-        self.dType = demandType
-        self.tStart = tStart
-        self.tEnd = tEnd
-        self.demand = demand
-        self.dur = dur
-
-    def __str__(self):
-        return f"Patient: {self.name}\n  Location: {self.locX, self.locY}\n  Demand: {self.dType.name}\n  Capacity: {self.dType.capacity}\n  Duration: {self.dur}\n  Start time: {self.tStart}\n  End time: {self.tEnd}"
-
-class Depot():
-    def __init__(self, name, locX, locY):
-        self.name = str(name) + '_D'
-        self.loc = np.array([locX,locY])
-
-    def __str__(self):
-        return f"Depot: {self.name}\n Location: {self.loc}"
-
-class Node():
-    def __init__(self, name, type, loc):
-        self.name = name
-        self.type = type
-        self.loc = loc
-
-    def __str__(self):
-        return f"Node: {self.name}\n Type: {self.type}\n Location: {self.loc}"
-
-# Read data
-filename = './patients.xlsx'
-ws = pd.read_excel(filename, sheet_name='Patients')
-patients = []
-for i, row in ws.iterrows():
-    this = Patient(*row)
-    patients.append(this)
-
-ws = pd.read_excel(filename, sheet_name='Depots')
-depots = []
-for i,row in ws.iterrows():
-    this = Depot(*row)
-    depots.append(this)
-
-ws = pd.read_excel(filename, sheet_name='Vehicles')
-vehicles = []
-for i,row in ws.iterrows():
-    this = Vehicle(*row)
-    vehicles.append(this)
-
-nodes = []
-for row in patients:
-    this = Node(row.name, 'Patient', row.loc)
-    nodes.append(this)
-for row in depots:
-    this = Node(row.name, 'Depot', row.loc)
-    nodes.append(this)
-
-
-w = 4   # of scenarios
-scenarios = range(w)
-p = [1/w for i in scenarios]        # probability
-
-# sets
-K = [k.name for k in vehicles]
-J = [j.name for j in patients]
-D = [d.name for d in depots]
-N = [n.name for n in nodes]
-
-# parameters
-cap = {k.name: k.cap for k in vehicles}             # capacity
-totOprT = {k.name: k.totOprTime for k in vehicles}  # total operation time
-oprCost = {k.name: k.oprCost for k in vehicles}     # operation cost
-demand = {i.name: i.demand for i in patients}
-tStart = {i.name: i.tStart for i in patients}
-tEnd = {i.name: i.tEnd for i in patients}
-
-dist = {(l.name,l.name):0 for l in nodes}                     # distance between 2 nodes
-for i, l1 in enumerate(nodes):
-    for j, l2 in enumerate(nodes):
-        if i < j:
-            dist[l1.name,l2.name] = np.linalg.norm(l1.loc-l2.loc)
-            dist[l2.name,l1.name] = dist[l1.name,l2.name]
-traCost = {(i.name, j.name):1 for i in nodes for j in nodes}      # travel cost
-
-    # travel time
-travelT = {(i,j,w):dist[i,j]*60/50 for i,j in dist for w in scenarios}
-for w in scenarios:
-    speed = np.random.normal(50)
-    for i,j in dist:
-        travelT[i,j] = dist[i,j]*60/speed
-traTMean = {(i,j):dist[i,j]*60/50 for i,j in dist} 
-  # service time
-svcT = {(i.name,w): i.dur for i in patients for w in scenarios}
-svcTMean = {i.name: i.dur for i in patients}
-
-waitingPenalty = 2
-idlePenalty = 1
-overtimePenalty = 10
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 
 def FirstStage():
+    # sets
+    K = [k.name for k in vehicles]
+    J = [j.name for j in patients]
+    D = [d.name for d in depots]
+    N = [n.name for n in nodes]
     try:
     # Create a new model
         model = gp.Model ("FSMILP")
@@ -136,33 +38,33 @@ def FirstStage():
         u = model.addVars(J, lb = 0.0, name = "u")                          # planned arrival time at i
         s = model.addVars(N, N, K, vtype = GRB.BINARY, name = "s")          # assign arc (i,j) to veh k
 
-        O = model.addVars(K, lb = 0.0, name = "O")                          # overtime of veh k
-        I = model.addVars(N, K, lb = 0.0, name = "I")                       # idletime at patient i of veh k
-        W = model.addVars(N, K, lb = 0.0, name = "W")                       # waitingtime at patient i of veh k
+        O = model.addVars(K, scenarios, lb = 0.0, name = "O")                          # overtime of veh k
+        I = model.addVars(N, K, scenarios, lb = 0.0, name = "I")                       # idletime at patient i of veh k
+        W = model.addVars(N, K, scenarios, lb = 0.0, name = "W")                       # waitingtime at patient i of veh k
         model.update()
     # Set objective
         objective = gp.quicksum(oprCost[i]*x[i] for i in K)\
-             + gp.quicksum(p[w]*(gp.quicksum(traCost[i,j]*travelT[i,j,w]*s[i,j,k] for i in N for j in N for k in K) + gp.quicksum(waitingPenalty*W[i,k] + idlePenalty*I[i,k] + overtimePenalty*O[k] for k in K for i in N)) for w in scenarios)
+             + gp.quicksum(p[w]*(gp.quicksum(traCost[i,j]*travelT[i,j,w]*s[i,j,k] for i in N for j in N for k in K) + gp.quicksum(waitingPenalty*W[i,k,w] + idlePenalty*I[i,k,w] + overtimePenalty*O[k,w] for k in K for i in N)) for w in scenarios)
         model.setObjective(objective, GRB.MINIMIZE)
 
     # Add constraints
         model.addConstrs((gp.quicksum(z[k,d] for d in D)==1 for k in K), name = "assign-1")
         model.addConstrs((gp.quicksum(y[i,k] for k in K)==1 for i in J), name = "assign-2")
-        model.addConstrs((y[i,k] <=x[k] for i in J for k in K), name = "assign-3")
+        # model.addConstrs((y[i,k] <=x[k] for i in J for k in K), name = "assign-3")
         model.addConstrs((gp.quicksum(s[i,j,k] for j in N) == y[i,k] for k in K for i in J), name="tour1")
         model.addConstrs((gp.quicksum(s[i,j,k] for i in N) == y[j,k] for k in K for j in J), name="tour2")
         model.addConstrs((gp.quicksum(s[d,i,k] for i in J)==z[k,d]*x[k] for k in K for d in D), name = "samedepot1")
         model.addConstrs((gp.quicksum(s[i,d,k] for i in J)==z[k,d]*x[k] for k in K for d in D), name = "samedepot2")
-        model.addConstrs((gp.quicksum(s[i,j,k]*demand[i] for i in J for j in J if j != i)<=cap[k]*x[k] for k in K), name = "capacity")
+        model.addConstrs((gp.quicksum(y[i,k]*demand[i] for i in J)<=cap[k]*x[k] for k in K), name = "capacity")
         model.addConstrs((tStart[i]<=u[i] for i in J), name = "timewindow-1")
         model.addConstrs((u[i]<=tEnd[i] for i in J), name = "timewindow-2")
         M = {(i,j) : 24*60 + svcTMean[i] + traTMean[i,j] for i in J for j in J}
         model.addConstrs((u[j]>=u[i]+traTMean[i,j]+svcTMean[i]-M[i,j]*(1-gp.quicksum(s[i,j,k] for k in K)) for i in J for j in J), name = "planned_arri_time")
 
         M = 60*60
-        model.addConstrs((W[j,k]>=(travelT[i,j,w]+svcT[i,w]+W[i,k]+I[i,k])-M*(1-s[i,j,k])-u[j]+u[i] for i in J for j in J for k in K for w in scenarios), name = "waitingtime-1")
-        model.addConstrs((O[k]>=(travelT[i,d,w]+svcT[i,w]+W[i,k]+I[i,k])-M*(1-s[i,d,k])-totOprT[k]+u[i] for i in J for d in D for k in K for w in scenarios), name = "overtime")
-        model.addConstrs((W[i,k]>=(travelT[d,i,w]+I[d,k])-M*(1-s[d,i,k])-u[i] for i in J for d in D for k in K for w in scenarios), name = "waitingtime-2")
+        model.addConstrs((W[j,k,w]>=(travelT[i,j,w]+svcT[i,w]+W[i,k,w]+I[i,k,w])-M*(1-s[i,j,k])-u[j]+u[i] for i in J for j in J for k in K for w in scenarios), name = "waitingtime-1")
+        model.addConstrs((O[k,w]>=(travelT[i,d,w]+svcT[i,w]+W[i,k,w]+I[i,k,w])-M*(1-s[i,d,k])-totOprT[k]+u[i] for i in J for d in D for k in K for w in scenarios), name = "overtime")
+        model.addConstrs((W[i,k,w]>=(travelT[d,i,w]+I[d,k,w])-M*(1-s[d,i,k])-u[i] for i in J for d in D for k in K for w in scenarios), name = "waitingtime-2")
 
     # Optimize model
         model.optimize()
@@ -194,7 +96,7 @@ def FirstStage():
                 while True:
                     for i in patients:
                         if s[cur,i.name,k.name].x > 0.5:
-                            route += " -> {} (dist={:.2f}, t={:.2f}, proc={})".format(i.name,dist[cur,i.name],u[i.name].x,i.dur)
+                            route += " -> {} (dist={:.2f}, t={:.2f}, proc={:.2f})".format(i.name,dist[cur,i.name],u[i.name].x,i.dur)
                             cur = i.name
                     for j in D:
                         if s[cur,j,k.name].x > 0.5:
@@ -218,6 +120,11 @@ def FirstStage():
         pass
 
 def SecondStage(u,s,x,w):
+    # sets
+    K = [k.name for k in vehicles]
+    J = [j.name for j in patients]
+    D = [d.name for d in depots]
+    N = [n.name for n in nodes]
     try:
     # Create a new model
         model = gp.Model ("SSMILP")
@@ -228,7 +135,7 @@ def SecondStage(u,s,x,w):
         W = model.addVars(N, K, lb = 0.0, name = "W")                    # waitingtime at patient i of veh k
 
     # Set objective
-        objective = gp.quicksum(traCost[i,j]*travelT[i,j,w]*s[i,j,k] for i in N for j in N for k in K) + gp.quicksum(waitingPenalty*W[i,k] + idlePenalty*I[i,k] + overtimePenalty*O[k] for k in K for i in N)
+        objective = gp.quicksum(oprCost[i]*x[i] for i in K) + gp.quicksum(traCost[i,j]*travelT[i,j,w]*s[i,j,k] for i in N for j in N for k in K) + gp.quicksum(waitingPenalty*W[i,k] + idlePenalty*I[i,k] + overtimePenalty*O[k] for k in K for i in N)
         model.setObjective(objective, GRB.MINIMIZE)
 
     # Add constraints
@@ -261,9 +168,156 @@ def printScen(scenStr):
     sLen = len(scenStr)
     print("\n" + "*"*sLen + "\n" + scenStr + "\n" + "*"*sLen + "\n", file=f)
 
-# Test Model
+### Helper Classes
+class Vehicle():
+    def __init__(self, name, cap, totOprTime, oprCost):
+        self.name = name
+        self.cap = cap
+        self.totOprTime = totOprTime
+        self.oprCost = oprCost
+
+    def __str__(self):
+        return f"Vehicle: {self.name}\n  Capacity: {self.cap}\n  Total Operation Time: {self.totOprTime}\n \
+            Operation Cost: {self.oprCost}"
+
+class Demand():
+    def __init__(self, name, capacity, duration):
+        self.name = name
+        self.capacity = capacity
+        self.dur = duration
+
+    def __str__(self):
+        about = f"Demand: {self.name}\n  Capacity: {self.capacity}\n  Duration: {self.dur}"
+        return about
+
+class Patient():
+    def __init__(self, name, locX, locY, tStart, tEnd, demandType, demand, dur):
+        self.name = name
+        self.loc = [locX,locY]
+        self.dType = demandType
+        self.tStart = tStart
+        self.tEnd = tEnd
+        self.demand = demand
+        self.dur = dur
+
+    def __str__(self):
+        return f"Patient: {self.name}\n  Location: {self.locX, self.locY}\n  Demand: {self.dType.name}\n  Capacity: {self.dType.capacity}\n  Duration: {self.dur}\n  Start time: {self.tStart}\n  End time: {self.tEnd}"
+
+class Depot():
+    def __init__(self, name, locX, locY):
+        self.name = name
+        self.loc = [locX,locY]
+
+    def __str__(self):
+        return f"Depot: {self.name}\n Location: {self.loc}"
+
+class Node():
+    def __init__(self, name, type, loc):
+        self.name = name
+        self.type = type
+        self.loc = loc
+
+    def __str__(self):
+        return f"Node: {self.name}\n Type: {self.type}\n Location: {self.loc}"
+
+
+# Read data
+filename = './data.xlsx'
+ws = pd.read_excel(filename, sheet_name='Patients')
+patients = []
+for i, row in ws.iterrows():
+    this = Patient(*row)
+    patients.append(this)
+
+ws = pd.read_excel(filename, sheet_name='Depots')
+depots = []
+for i,row in ws.iterrows():
+    this = Depot(*row)
+    depots.append(this)
+
+ws = pd.read_excel(filename, sheet_name='Vehicles')
+vehicles = []
+for i,row in ws.iterrows():
+    this = Vehicle(*row)
+    vehicles.append(this)
+
+nodes = []
+for row in patients:
+    this = Node(row.name, 'Patient', row.loc)
+    nodes.append(this)
+for row in depots:
+    this = Node(row.name, 'Depot', row.loc)
+    nodes.append(this)
+
+# parameters
+scenarios = range(5)                                # scenarios
+p = [1/len(scenarios) for i in scenarios]           # probability
+
+cap = {k.name: k.cap for k in vehicles}             # capacity
+totOprT = {k.name: k.totOprTime for k in vehicles}  # total operation time
+oprCost = {k.name: k.oprCost for k in vehicles}     # operation cost
+demand = {i.name: i.demand for i in patients}
+tStart = {i.name: i.tStart for i in patients}
+tEnd = {i.name: i.tEnd for i in patients}
+
+dist = {(l.name,l.name):0 for l in nodes}                     # distance between 2 nodes
+for i, l1 in enumerate(nodes):
+    for j, l2 in enumerate(nodes):
+        if i < j:
+            dist[l1.name,l2.name] = haversine(*l1.loc,*l2.loc)
+            dist[l2.name,l1.name] = dist[l1.name,l2.name]
+traCost = {(i.name, j.name):1 for i in nodes for j in nodes}      # travel cost
+
+sig = 0.2
+travelT = {(i,j,w):dist[i,j] for i,j in dist for w in scenarios}
+svcT = {(i.name,w): i.dur for i in patients for w in scenarios}
+for w in scenarios:
+    # sampling travel times for each arc
+    samp = range(3)
+    case = np.random.choice(samp,size=1,p=[0.8,0.15,0.05])
+    if case == 0:
+        speed = np.random.normal(60,60*sig)
+    elif case == 1:
+        speed = np.random.normal(50,50*sig)
+    else:
+        speed = np.random.normal(30,30*sig)
+    for i,j in dist:
+        travelT[i,j] = dist[i,j]*60/speed
+
+    # sampling service times for each patient
+    samp = range(4)
+    for i in patients:
+        case = np.random.choice(samp,size=1,p=[0.3,0.05,0.5,0.15])
+        if case == 0:
+            svcT[i.name,w] = np.random.exponential(45)
+        elif case == 1:
+            svcT[i.name,w] = np.random.exponential(60)
+        elif case == 2:
+            svcT[i.name,w] = np.random.exponential(30)
+        else:
+            svcT[i.name,w] = np.random.exponential(90)
+
+traTMean = {(i,j):dist[i,j]*60/(60*0.8+50*0.15+30*0.05) for i,j in dist} 
+
+for i in patients:
+    temp = 0
+    for w in scenarios:
+        temp += svcT[i.name,w]
+    i.dur = temp/len(scenarios)
+
+svcTMean = {i.name: i.dur for i in patients}
+
+waitingPenalty = 2
+idlePenalty = 1
+overtimePenalty = 10
+
+
+# Run Model
 f = open("output.txt", "w")
 printScen("Solving First-Stage Model")
+start_time = time.time()
 u,s,x = FirstStage()
 printScen("Solving Second-Stage Model")
 SecondStage(u,s,x,1)
+end_time = time.time()
+printScen("time taken = "+str(end_time-start_time))
