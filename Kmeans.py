f2bfb3fc236055f4f55feadeadfbd53e614aac82
zhuxiaoy1997 <60758272+zhuxiaoy1997@users.noreply.github.com>
2021-12-16 02:33:48 +0800

    Add files via upload
    Update Kmeans about depot patients assignment

--- Kmeans.py
+++ Kmeans.py
@@ -6,6 +6,8 @@ import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 import Project as p
+import benders
+
 # # suppress all warnings
 # import warnings
 # warnings.filterwarnings("ignore")
@@ -26,18 +28,16 @@ class DecisionVar:
         self.I = {}
         self.W= {}
 
-decisionVar = DecisionVar()
-
 
 def assign_PatientDepotVehicle(max_iter=500):
     # parameter: max_iter
     # this is the max iteration kmeans will run for each depot
     ############### assign patients to depots ######################
     # load in depots and patients information and covert it to dataframes
-    depots = p.sets.depot
-    patients = p.sets.patient
+    depots = sets.depot
+    patients = sets.patient
     depot_df = pd.DataFrame(columns=['name', 'locX', 'locY'])
-    patient_df = pd.DataFrame(columns=['name', 'locX', 'locY'])
+    patient_df = pd.DataFrame(columns=['name', 'locX', 'locY', 'tStart', 'tEnd'])
     for d in depots:
         depot_df = depot_df.append({'name': d.name, 'locX': d.loc[0], 'locY': d.loc[1]}, ignore_index=True)
     for pa in patients:
@@ -47,17 +47,24 @@ def assign_PatientDepotVehicle(max_iter=500):
     stand_D = StandardScaler().fit_transform(depot_df[['locX', 'locY']])
     stand_P = StandardScaler().fit_transform(patient_df[['locX', 'locY']])
     
-    # let depots to be our centroids
-    centroids = stand_D
-    kmeans = KMeans(n_clusters=len(depots), init=centroids, max_iter=1) # just run one k-Means iteration so that the centroids are not updated
+    # use Kmeans to cluster patients (n_cluster = depot number)    
+    kmeans = KMeans(n_clusters=len(depots), max_iter=max_iter) 
     kmeans.fit(stand_P)
 
-    # assign patients to depots and store the result as a dictionary
-    patient_df['depot'] = kmeans.labels_
+    ## after clustering, find the nearest depot for each centroid
+    label_depot = {} # key is kmeans center, value is depot
+    left_D = stand_D.copy()
+    #for ind, c in pd.DataFrame(kmeans.cluster_centers_).drop([2, 3],axis=1).iterrows():
+    for ind, c in enumerate(kmeans.cluster_centers_):
+        min_dist = min([np.linalg.norm(d - c) for d in left_D])
+        label_depot[ind] = [ind for ind, d in enumerate(stand_D) if np.linalg.norm(d - c) == min_dist][0]
+        left_D = np.delete(left_D, [np.linalg.norm(d - c) for d in left_D].index(min_dist), axis = 0)
+    ## assign patients to depots
+    patient_df['depot'] = [label_depot[l] for l in kmeans.labels_] ## depot number
 
     ########################################## assign vehicles to depots ########################################
     # load in vehicles information and covert it to dataframes
-    vehicles = p.sets.vehicle
+    vehicles = sets.vehicle
     vehicle_df = pd.DataFrame(columns=['name', 'cap', 'totOprTime', 'oprCost'])
     for v in vehicles:
         vehicle_df = vehicle_df.append({'name': v.name, 'cap': v.cap, 'totOprTime': v.totOprTime, 'oprCost': v.oprCost}, ignore_index=True)
@@ -94,6 +101,7 @@ def assign_PatientDepotVehicle(max_iter=500):
             for veh in allVehicles:
                 sort_veh = vehicle_df.loc[vehicle_df['name'].isin(allVehicles)].sort_values(['cap']).reset_index(drop=True)
                 maxCap = sort_veh.loc[len(sort_veh)-1, 'cap']
+                #minCap = sort_veh.loc[0, 'cap']
 
             Depotkmeans = KMeans(n_clusters=maxCap, max_iter=max_iter) # do kmeans using the max capacity
             # standardize data
@@ -155,7 +163,7 @@ def cal_IWReal(EA, wholeTime):
 
 # calculate idle, waiting, overtime
 def cal_IWO(routes):
-    for w in p.sets.scenario:
+    for w in sets.scenario:
         for veh, route in routes.items():
             depotName = [pair[1] for pair in decisionVar.z.keys() if pair[0] == veh][0]
             RA = 0 # to store real arrival time for each patient. start from 0. 
@@ -163,56 +171,56 @@ def cal_IWO(routes):
                 EA = route.loc[position, 'tStart']
                 LA = route.loc[position, 'tEnd']
                 if position == 0: # if it's the first patient
-                    wholeTime = RA + p.para.travelT[depotName, patient['name'], w]
+                    wholeTime = RA + para.travelT[depotName, patient['name'], w]
                     decisionVar.I[depotName, veh, w], decisionVar.W[patient['name'], veh, w], RA = cal_IWReal(EA, wholeTime)
                 else:
-                    wholeTime = RA + p.para.svcT[route.loc[position-1, 'name'],w] + p.para.travelT[route.loc[position-1, 'name'], patient['name'], w]
+                    wholeTime = RA + para.svcT[route.loc[position-1, 'name'],w] + para.travelT[route.loc[position-1, 'name'], patient['name'], w]
                     decisionVar.I[route.loc[position-1, 'name'], veh, w], decisionVar.W[patient['name'], veh, w], RA = cal_IWReal(EA, wholeTime)
             # after finishing the for loop of one route dataframe, calculate the overtime
             # idle time for the last patient should be zero
-            decisionVar.O[veh, w] = max(RA + 0 + p.para.svcT[route.loc[len(route)-1, 'name'],w] + p.para.travelT[route.loc[len(route)-1, 'name'], depotName, w] - p.sets.vehicle[veh].totOprTime , 0)
+            decisionVar.O[veh, w] = max(RA + 0 + para.svcT[route.loc[len(route)-1, 'name'],w] + para.travelT[route.loc[len(route)-1, 'name'], depotName, w] - sets.vehicle[veh].totOprTime , 0)
     pass
 
 # calculate total cost
 def cal_ToTCost():
     ToTOperCost = 0
-    for veh in p.sets.K:
+    for veh in sets.K:
         try:
-            ToTOperCost = ToTOperCost + p.para.oprCost[veh]*decisionVar.x[veh]
+            ToTOperCost = ToTOperCost + para.oprCost[veh]*decisionVar.x[veh]
         except:
             continue
 
     ToTtravCost = 0
-    for w in p.sets.scenario:
+    for w in sets.scenario:
         for event in decisionVar.s:
             i = event[0]
             j = event[1]
             veh = event[2]
-            ToTtravCost = ToTtravCost + p.sets.p[w]*(p.para.traCost[i,j] * p.para.travelT[i,j,w] * decisionVar.s[i,j,veh])
+            ToTtravCost = ToTtravCost + sets.p[w]*(para.traCost[i,j] * para.travelT[i,j,w] * decisionVar.s[i,j,veh])
 
     ToTIWO = 0
-    for w in p.sets.scenario:
+    for w in sets.scenario:
         for event in decisionVar.W:
             i = event[0]
             veh = event[1]
-            ToTIWO = ToTIWO + p.sets.p[w]*p.para.waitingPenalty*decisionVar.W[i,veh,w] 
+            ToTIWO = ToTIWO + sets.p[w]*para.waitingPenalty*decisionVar.W[i,veh,w] 
         
         for event in decisionVar.I:
             i = event[0]
             veh = event[1]
-            ToTIWO = ToTIWO + p.sets.p[w]*p.para.idlePenalty*decisionVar.I[i,veh,w]
+            ToTIWO = ToTIWO + sets.p[w]*para.idlePenalty*decisionVar.I[i,veh,w]
             
         for event in decisionVar.O:
             veh = event[0]
-            ToTIWO = ToTIWO + p.sets.p[w]*p.para.overtimePenalty*decisionVar.O[veh,w]
+            ToTIWO = ToTIWO + sets.p[w]*para.overtimePenalty*decisionVar.O[veh,w]
 
     ToTCost = ToTOperCost + ToTtravCost + ToTIWO
     return ToTCost
 
 # print results to files
-def printResult(routes, ToTCost):
+def printResult(routes, ToTCost, max_iter=500):
     # Assignments
-    vehicles = p.sets.vehicle
+    vehicles = sets.vehicle
     vehicle_df = pd.DataFrame(columns=['name', 'cap', 'totOprTime', 'oprCost'])
     for v in vehicles:
         vehicle_df = vehicle_df.append({'name': v.name, 'cap': v.cap, 'totOprTime': v.totOprTime, 'oprCost': v.oprCost}, ignore_index=True)
@@ -224,9 +232,9 @@ def printResult(routes, ToTCost):
         else:
             depotName = [pair[1] for pair in decisionVar.z.keys() if pair[0] == k][0]
             vehStr = "Vehicle {} assigned to depot {}.".format(k, depotName)
-        print(vehStr, file=p.sets.f1)
+        print(vehStr, file=sets.f)
     # Routing
-    print("",file=p.sets.f1)
+    print("",file=sets.f)
     for k in Vehi_used:
         depotName = [pair[1] for pair in decisionVar.z.keys() if pair[0] == k][0]
         routeStr = str(depotName)
@@ -236,23 +244,54 @@ def printResult(routes, ToTCost):
                     
         routeStr += " -> {} ".format(depotName)
                     
-        print("Vehicle {}'s route: {}".format(k, routeStr),file=p.sets.f1)
+        print("Vehicle {}'s route: {}".format(k, routeStr),file=sets.f)
     
     # Objective Value
-    print("",file=p.sets.f1)
-    print("Kmeans Iterations: {}".format(max_iter), file=p.sets.f1)
-    print("Objective Value: {:.4f}".format(ToTCost),file=p.sets.f1)
-    print("Average Idle Time: {:.2f}".format(sum(decisionVar.I.values())/len(decisionVar.I.values())),file=p.sets.f1)
-    print("Average Waiting Time: {:.2f}".format(sum(decisionVar.W.values())/len(decisionVar.W.values())),file=p.sets.f1)
-    print("Average Over Time: {:.2f}".format(sum(decisionVar.O.values())/len(decisionVar.O.values())),file=p.sets.f1)
+    print("",file=sets.f)
+    print("Kmeans Iterations: {}".format(max_iter), file=sets.f)
+    print("Objective Value: {:.4f}".format(ToTCost),file=sets.f)
+    print("Average Idle Time: {:.2f}".format(sum(decisionVar.I.values())/len(decisionVar.I.values())),file=sets.f)
+    print("Average Waiting Time: {:.2f}".format(sum(decisionVar.W.values())/len(decisionVar.W.values())),file=sets.f)
+    print("Average Over Time: {:.2f}".format(sum(decisionVar.O.values())/len(decisionVar.O.values())),file=sets.f)
 
     
-p.printScen("Solving the problem using Kmeans Heuristic",p.sets.f1)
-start_time = time.time()
-max_iter = 500
-routes = assign_PatientDepotVehicle(max_iter = max_iter)
-cal_IWO(routes)
-ToTCost = cal_ToTCost()
-printResult(routes, ToTCost)
-end_time = time.time()
-p.printScen("time taken = "+str(np.round(end_time-start_time,4)) + 's',p.sets.f1)
+# p.printScen("Solving the problem using Kmeans Heuristic",sets.f)
+# start_time = time.time()
+# max_iter = 500
+# routes = assign_PatientDepotVehicle(max_iter = max_iter)
+# cal_IWO(routes)
+# ToTCost = cal_ToTCost()
+# printResult(routes, ToTCost)
+# end_time = time.time()
+# p.printScen("time taken = "+str(np.round(end_time-start_time,4)) + 's',sets.f)
+
+for i in range(3,6):
+    for j in [10,20,30]:
+        sets = p.Sets("({}-{})".format(i,j),10)
+        para = p.Parameters(sets)
+        decisionVar = DecisionVar()
+
+
+        # p.printScen("Solving TSMILP Model using Benders' Decomposition",sets.f)
+        # start_time = time.time()
+        # m = benders.MasterProblem(para,sets)
+        # m.optimize()
+        # end_time = time.time()
+        # p.printScen("time taken = "+str(end_time-start_time),sets.f)
+
+        # p.printScen("Solving TSMILP Model",sets.f)
+        # start_time = time.time()
+        # m = p.TSMILP(sets,para)
+        # m.optimize()
+        # end_time = time.time()
+        # p.printScen("time taken = "+str(end_time-start_time),sets.f)
+
+        p.printScen("Solving the problem using Kmeans Heuristic",sets.f)
+        start_time = time.time()
+        max_iter = 500
+        routes = assign_PatientDepotVehicle(max_iter = max_iter)
+        cal_IWO(routes)
+        ToTCost = cal_ToTCost()
+        printResult(routes, ToTCost)
+        end_time = time.time()
+        p.printScen("time taken = "+str(end_time-start_time) + 's',sets.f)
